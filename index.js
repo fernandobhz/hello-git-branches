d9dc8e4bf93f628429c494be1d0d541bcac7d4c6
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-07-07 19:55:14 -0300

    chore: creating a variable to hold the port number

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send(`
@@ -8,5 +9,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Server is up and running on port 3000");
+  console.log(`Server is up and running on port ${port}`);
 });
